0de5e867e3ba8d2b61e941bea2480314187e7cc6
M-Str <miroslav.stransky@live.com>
2025-02-02 13:10:15 +0100

    MOD: přidání SWAGGERREsponse

--- cbData.BE.BusinessLogic/Controllers/ReportController.cs
+++ cbData.BE.BusinessLogic/Controllers/ReportController.cs
@@ -4,11 +4,15 @@ using cbData.BE.DB.Services;
 using cbData.Shared.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace cbData.BE.BusinessLogic.Controllers
 {
 	[ApiController]
 	[ApiExplorerSettings(GroupName = "v1")]
+	[SwaggerResponse(200, "Úspěšné získání položky/položek [Další informace](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200)")]
+	[SwaggerResponse(404, "Položka/Položky nenalezeny.[Další informace](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404)")]
+	[SwaggerResponse(500, "Chyba serveru.[Další informace](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500)")]
 	public class ReportController : ControllerBase
 	{
 		private ReportDbService _reportDbService;
